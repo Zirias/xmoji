e5fd711e506e1263661b6e23e515a07694f47976
Felix Palmen <felix@palmen-it.de>
2024-05-08 21:16:11 +0200

    Xmoji: cleanup code

--- src/bin/xmoji/xmoji.c
+++ src/bin/xmoji/xmoji.c
@@ -19,6 +19,25 @@ static void onclose(void *receiver, void *sender, void *args)
     PSC_Service_quit();
 }
 
+static void onprestartup(void *receiver, void *sender, void *args)
+{
+    (void)receiver;
+    (void)sender;
+
+    PSC_EAStartup *ea = args;
+
+    x11 = X11Adapter_create();
+    if (!x11 || !(win = Window_create(x11)))
+    {
+	PSC_EAStartup_return(ea, EXIT_FAILURE);
+	return;
+    }
+
+    Window_setSize(win, 640, 200);
+    Window_setTitle(win, "Xmoji test");
+    PSC_Event_register(Window_closed(win), 0, onclose, 0);
+}
+
 static void onstartup(void *receiver, void *sender, void *args)
 {
     (void)receiver;
@@ -35,32 +54,19 @@ static void onshutdown(void *receiver, void *sender, void *args)
     (void)sender;
     (void)args;
 
+    Object_destroy(win);
+    X11Adapter_destroy(x11);
     PSC_Log_setAsync(0);
 }
 
 SOLOCAL int Xmoji_run(void)
 {
-    int rc = EXIT_FAILURE;
+    PSC_RunOpts_foreground();
     PSC_Log_setFileLogger(stderr);
     PSC_Log_setMaxLogLevel(PSC_L_DEBUG);
-
-    x11 = X11Adapter_create();
-    if (!x11) goto done;
-    win = Window_create(x11);
-    if (!win) goto done;
-    Window_setSize(win, 640, 200);
-    Window_setTitle(win, "Xmoji test");
-
-    PSC_RunOpts_init(0);
-    PSC_RunOpts_foreground();
+    PSC_Event_register(PSC_Service_prestartup(), 0, onprestartup, 0);
     PSC_Event_register(PSC_Service_startup(), 0, onstartup, 0);
     PSC_Event_register(PSC_Service_shutdown(), 0, onshutdown, 0);
-    PSC_Event_register(Window_closed(win), 0, onclose, 0);
-    rc = PSC_Service_run();
-
-done:
-    Object_destroy(win);
-    X11Adapter_destroy(x11);
-    return rc;
+    return PSC_Service_run();
 }
 
