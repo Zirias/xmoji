37a90f81e9106cfb17a8abeb8afc7c6cfa8cabb2
Felix Palmen <felix@palmen-it.de>
2024-08-05 19:15:04 +0200

    SpinBox: Add spin buttons (up/down)

--- src/bin/xmoji/spinbox.c
+++ src/bin/xmoji/spinbox.c
@@ -68,10 +68,35 @@ static void expose(void *obj, Rect region)
 
 static int draw(void *obj, xcb_render_picture_t picture)
 {
-    (void)picture;
-
     SpinBox *self = Object_instance(obj);
-    return Widget_draw(self->textBox);
+    int rc = Widget_draw(self->textBox);
+    if (rc == 0 && picture)
+    {
+	xcb_connection_t *c = X11Adapter_connection();
+	Rect geom = Widget_geometry(self->textBox);
+	geom.pos.x += geom.size.width;
+	geom.size.width = geom.size.height / 2;
+	xcb_rectangle_t uprect = {
+	    geom.pos.x + 1, geom.pos.y + 1,
+	    geom.size.width - 2, geom.size.width - 1 };
+	Color upcol = Widget_color(self,
+		self->hover == SBH_UP ? COLOR_BG_ACTIVE : COLOR_BG_ABOVE);
+	CHECK(xcb_render_fill_rectangles(c, XCB_RENDER_PICT_OP_OVER,
+		    picture, Color_xcb(upcol), 1, &uprect),
+		"Cannot draw up button background for 0x%x",
+		(unsigned)picture);
+	xcb_rectangle_t downrect = {
+	    geom.pos.x + 1, geom.pos.y + geom.size.width + 1,
+	    geom.size.width - 2, geom.size.width - 1
+		+ !(geom.size.height & 1)};
+	Color downcol = Widget_color(self,
+		self->hover == SBH_DOWN ? COLOR_BG_ACTIVE : COLOR_BG_ABOVE);
+	CHECK(xcb_render_fill_rectangles(c, XCB_RENDER_PICT_OP_OVER,
+		    picture, Color_xcb(downcol), 1, &downrect),
+		"Cannot draw down button background for 0x%x",
+		(unsigned)picture);
+    }
+    return rc;
 }
 
 static Size minSize(const void *obj)
@@ -96,6 +121,14 @@ static void leave(void *obj)
 {
     SpinBox *self = Object_instance(obj);
     leavetb(self);
+    if (self->hover != SBH_NONE)
+    {
+	self->hover = SBH_NONE;
+	Rect geom = Widget_geometry(self->textBox);
+	geom.pos.x += geom.size.width;
+	geom.size.width = geom.size.height / 2;
+	Widget_invalidateRegion(self, geom);
+    }
 }
 
 static void unselect(void *obj)
@@ -145,7 +178,36 @@ static Widget *childAt(void *obj, Pos pos)
 static int clicked(void *obj, const ClickEvent *event)
 {
     SpinBox *self = Object_instance(obj);
-    return Widget_clicked(self->textBox, event);
+    Rect geom = Widget_geometry(self->textBox);
+    if (Rect_containsPos(geom, event->pos))
+    {
+	return Widget_clicked(self->textBox, event);
+    }
+    if (event->button != MB_LEFT) return 0;
+    geom.pos.x += geom.size.width;
+    geom.size.width = geom.size.height / 2;
+    geom.size.height /= 2;
+    int value = self->val;
+    int step = self->step;
+    if (Rect_containsPos(geom, event->pos))
+    {
+	if (self->max - self->val < step) step = self->max - self->val;
+	value += step;
+    }
+    else
+    {
+	if (self->val - self->min < step) step = self->val - self->min;
+	value -= step;
+    }
+    if (value != self->val)
+    {
+	self->val = value;
+	UniStr *str = valstr(value);
+	TextBox_setText(self->textBox, str);
+	UniStr_destroy(str);
+	PSC_Event_raise(self->changed, 0, &self->val);
+    }
+    return 1;
 }
 
 static void layoutChanged(void *receiver, void *sender, void *args)
@@ -262,6 +324,8 @@ SpinBox *SpinBox_createBase(void *derived, const char *name,
 	    textChanged, 0);
 
     Widget_setPadding(self, (Box){0, 0, 0, 0});
+    Widget_setBackground(self, 1, COLOR_BG_BELOW);
+    Widget_setCursor(self, XC_HAND);
     PSC_Event_register(Widget_sizeChanged(self), self, layoutChanged, 0);
     PSC_Event_register(Widget_originChanged(self), self, layoutChanged, 0);
 
