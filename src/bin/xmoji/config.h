277c20ac0d09d2fc4114efb9cbbf1e5ebe448ea0
Felix Palmen <felix@palmen-it.de>
2024-08-06 19:19:46 +0200

    Config: Add properties for key injector

--- src/bin/xmoji/config.h
+++ src/bin/xmoji/config.h
@@ -1,7 +1,7 @@
 #ifndef XMOJI_CONFIG_H
 #define XMOJI_CONFIG_H
 
-#include <poser/decl.h>
+#include "keyinjector.h"
 
 #define HISTSIZE 64
 
@@ -16,8 +16,19 @@ typedef struct ConfigChangedEventArgs
 
 Config *Config_create(const char *path);
 
-EmojiHistory *Config_history(Config *self)
-    CMETHOD;
+EmojiHistory *Config_history(Config *self) CMETHOD;
+
+InjectorFlags Config_injectorFlags(const Config *self) CMETHOD;
+void Config_setInjectorFlags(Config *self, InjectorFlags flags) CMETHOD;
+PSC_Event *Config_injectorFlagsChanged(Config *self) CMETHOD ATTR_RETNONNULL;
+
+unsigned Config_waitBefore(const Config *self) CMETHOD;
+void Config_setWaitBefore(Config *self, unsigned ms) CMETHOD;
+PSC_Event *Config_waitBeforeChanged(Config *self) CMETHOD ATTR_RETNONNULL;
+
+unsigned Config_waitAfter(const Config *self) CMETHOD;
+void Config_setWaitAfter(Config *self, unsigned ms) CMETHOD;
+PSC_Event *Config_waitAfterChanged(Config *self) CMETHOD ATTR_RETNONNULL;
 
 void Config_destroy(Config *self);
 
