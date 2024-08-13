31812852ed4ec9be01a10c3a77c93685adb8edbf
Felix Palmen <felix@palmen-it.de>
2024-08-13 09:26:21 +0200

    xdgOpen: Fix checking dev-null fd

--- src/bin/xmoji/xdgopen.c
+++ src/bin/xmoji/xdgopen.c
@@ -50,7 +50,7 @@ int xdgOpen(const char *url)
     if (pid == 0)
     {
 	int devnull = open("/dev/null", O_RDWR);
-	if (devnull)
+	if (devnull >= 0)
 	{
 	    dup2(devnull, STDIN_FILENO);
 	    dup2(devnull, STDOUT_FILENO);
