0432932f6a449937d4b53acffaad58c8ba5920d5
Felix Palmen <felix@palmen-it.de>
2024-10-09 15:17:21 +0200

    xdgOpen: Error-check actual execution
    * Use intermediate process to be able to delegate the child to pid-1 if
      it keeps running
    * Use a self-pipe to be able to check whether exec() itself fails
    * Check child state twice a second for up to 3 seconds, if it's still
      running after that, assume success

--- src/bin/xmoji/xdgopen.c
+++ src/bin/xmoji/xdgopen.c
@@ -7,7 +7,10 @@
 #include <limits.h>
 #include <poser/core.h>
 #include <stdlib.h>
+#include <sys/select.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #ifndef PATH_MAX
@@ -64,16 +67,59 @@ static void doopen(void *arg)
     if (pid == 0)
     {
 	int devnull = open("/dev/null", O_RDWR);
-	if (devnull >= 0)
+	if (devnull < 0) exit(XOE_SYSTEM);
+	dup2(devnull, STDIN_FILENO);
+	dup2(devnull, STDOUT_FILENO);
+	dup2(devnull, STDERR_FILENO);
+	close(devnull);
+	if (setsid() < 0) exit(XOE_SYSTEM);
+	int pfd[2];
+	if (pipe(pfd) < 0) exit(XOE_SYSTEM);
+	if (fcntl(pfd[1], F_SETFD, FD_CLOEXEC) < 0) exit(XOE_SYSTEM);
+	if ((pid = fork()) < 0) exit(XOE_SYSTEM);
+
+	if (pid == 0)
 	{
-	    dup2(devnull, STDIN_FILENO);
-	    dup2(devnull, STDOUT_FILENO);
-	    dup2(devnull, STDERR_FILENO);
-	    close(devnull);
+	    close(pfd[0]);
+	    execl(tool, "xdg-open", openJob->url, NULL);
+	    char err = XOE_EXEC;
+	    write(pfd[1], &err, 1);
+	    exit(XOE_EXEC);
 	}
-	execl(tool, "xdg-open", openJob->url, NULL);
+
+	close(pfd[1]);
+	fd_set set;
+	FD_ZERO(&set);
+	FD_SET(pfd[0], &set);
+	struct timeval timeout;
+
+	for (int i = 0; i < 6; ++i)
+	{
+	    timeout.tv_sec = 0;
+	    timeout.tv_usec = 500000UL;
+	    int rc = select(pfd[0]+1, &set, 0, 0, &timeout);
+	    if (rc < 0) exit(XOE_SYSTEM);
+	    if (rc > 0) exit(XOE_EXEC);
+	    int status;
+	    pid_t cpid = waitpid(pid, &status, WNOHANG);
+	    if (cpid < 0) exit(XOE_SYSTEM);
+	    if (cpid == pid)
+	    {
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+		{
+		    exit(XOE_OK);
+		}
+		exit(XOE_SYSTEM);
+	    }
+	}
+	exit(XOE_OK);
     }
-    openJob->error = XOE_OK;
+
+    int status;
+    if (waitpid(pid, &status, 0) == pid && WIFEXITED(status))
+    {
+	openJob->error = WEXITSTATUS(status);
+    }
 }
 
 static void opendone(void *receiver, void *sender, void *args)
